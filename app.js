5b0985bebcf1ac8da520e7847c6d1da5c770ecfc
Nina Dmitrieva <nina.dmitrieva.spb>
2022-06-09 22:40:06 +0300

    test2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { PORT = 3000 } = process.env;
 const app = express();
+const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true, family: 4 });
 
@@ -18,6 +18,10 @@ app.use((req, _res, next) => {
 
 app.use(require('./routes/users'));
 app.use(require('./routes/cards'));
+
+app.all('*', (_req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
