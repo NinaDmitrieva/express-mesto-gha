0d71ac8e92e8484028d527657579d80c582e6552
Nina Dmitrieva <nina.dmitrieva.spb>
2022-06-11 18:54:13 +0300

    на ревью

--- controllers/users.js
+++ controllers/users.js
@@ -60,7 +60,12 @@ module.exports.updateUser = (req, res) => {
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Такого пользователя нет' });
+      }
+      return res.send({ data: user });
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(400).send({ message: `Введены некорректные данные: ${err.message}` });
