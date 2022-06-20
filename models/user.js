b4e8ff9fc40efcd7c038cfc95627d035932529f1
Nina Dmitrieva <nina.dmitrieva.spb>
2022-06-20 15:58:33 +0300

    validator

--- models/user.js
+++ models/user.js
@@ -1,30 +1,37 @@
-/* eslint-disable func-names */
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     default: 'Жак-Ив Кусто',
-    required: false,
     minlength: [2, 'Имя должно содержать больше 2 символов'],
     maxlength: [30, 'Слишком длинное имя, уложитесь в 30 символов'],
   },
   about: {
     type: String,
     default: 'Исследователь',
-    required: false,
     minlength: [2, 'Данное поле должно содержать не менее 2 символов'],
     maxlength: [30, 'Данное поле должно содержать не более 30 символов'],
   },
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    required: false,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Эта ссылка не подходит',
+    },
   },
   email: {
     type: String,
     required: [true, 'Обязательно заполните это поле'],
     unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: 'Такой email не подходит',
+    },
   },
   password: {
     type: String,
